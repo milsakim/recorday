11fc6b77df341c646055f0f6e3f341856555c1d8
samsamilsa <hyejeekim@me.com>
2022-03-01 09:13:41 +0900

    feat: Create TagListView
    TagListView is a UIView subclass for displaying tags.

---
+++ View/TagListView/TagListView.swift
@@ -0,0 +1,20 @@
+//
+//  TagListView.swift
+//  Recorday
+//
+//  Created by HyeJee Kim on 2022/03/01.
+//
+
+import UIKit
+
+class TagListView: UIView {
+    
+    var tagStrings: [String] = []
+    
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        
+    }
+
+}

e071bbf358de88bdb5b9b1fb4bb4a02e5d99e101
samsamilsa <hyejeekim@me.com>
2022-03-03 07:41:18 +0900

    feat: Add cell for RecordListViewController's table view

---
+++ View/DailyRecordTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  DailyRecordTableViewCell.swift
+//  Recorday
+//
+//  Created by HyeJee Kim on 2022/03/03.
+//
+
+import UIKit
+
+class DailyRecordTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}

6b81647e63ed9e97adb134848dbac46bff56b904
samsamilsa <hyejeekim@me.com>
2022-03-02 17:34:45 +0900

    feat: Create AddActivityCell

---
+++ View/Cells/AddActivityTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  AddActivityTableViewCell.swift
+//  Recorday
+//
+//  Created by HyeJee Kim on 2022/03/02.
+//
+
+import UIKit
+
+class AddActivityTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}

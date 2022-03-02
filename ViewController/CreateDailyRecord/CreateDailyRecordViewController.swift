a5ecd17912e17d4038a6a7d7e84e3693e4ade216
samsamilsa <hyejeekim@me.com>
2022-03-02 22:49:33 +0900

    feat: Add property to reference persistent container

--- ViewController/CreateDailyRecord/CreateDailyRecordViewController.swift
+++ ViewController/CreateDailyRecord/CreateDailyRecordViewController.swift
@@ -8,6 +8,18 @@
 import UIKit
 
 class CreateDailyRecordViewController: UIViewController {
+    
+    // MARK: - Property
+    
+    var persistentContainer: NSPersistentContainer?
+    
+    // MARK: - Deinit
+    
+    deinit {
+        if persistentContainer != nil {
+            persistentContainer = nil
+        }
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()

76cf7227e1f32951ec6bc16686f714fe0b0c6812
samsamilsa <hyejeekim@me.com>
2022-03-03 07:44:23 +0900

    feat: Make RecordListViewController conform to UITableViewDataSource

---
+++ ViewController/RecordListViewController/Extension/RecordListVC+UITableViewDataSource.swift
@@ -0,0 +1,20 @@
+//
+//  RecordListVC+UITableViewDataSource.swift
+//  Recorday
+//
+//  Created by HyeJee Kim on 2022/03/03.
+//
+
+import UIKit
+
+extension RecordListViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
+    
+}

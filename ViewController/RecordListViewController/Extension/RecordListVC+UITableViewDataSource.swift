3e63b30b097ad15e9e6996f7f98b8d4d65f19fd7
samsamilsa <hyejeekim@me.com>
2022-03-04 08:19:24 +0900

    feat: Populate cell's content

--- ViewController/RecordListViewController/Extension/RecordListVC+UITableViewDataSource.swift
+++ ViewController/RecordListViewController/Extension/RecordListVC+UITableViewDataSource.swift
@@ -17,10 +17,13 @@ extension RecordListViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         print(#function)
         if let cell: DailyRecordTableViewCell = tableView.dequeueReusableCell(withIdentifier: "DailyRecordTableViewCell", for: indexPath) as? DailyRecordTableViewCell {
-            cell.idLabel.text = self.dailyRecords[indexPath.row].id?.uuidString ?? "No ID"
             cell.moodLabel.text = self.dailyRecords[indexPath.row].mood
-            cell.activitiesLabel.text = self.dailyRecords[indexPath.row].activities?.compactMap({ $0 as? Activity }).reduce(into: "", { $0 += "\($1.title ?? "No tag") "})
-            cell.weatherLabel.text = self.dailyRecords[indexPath.row].weather
+            
+            let date: Date = Date(timeIntervalSince1970: self.dailyRecords[indexPath.row].timeStamp)
+            let dateFormatter: DateFormatter = DateFormatter()
+            dateFormatter.dateFormat = "yyyy-MM-dd 'at' HH:mm"
+            cell.timeLabel.text = dateFormatter.string(from: date)
+           
             return cell
         }
         

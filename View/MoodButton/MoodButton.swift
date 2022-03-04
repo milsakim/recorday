2ae929f3679a4657d178c279b5bddd0b1b1c746c
samsamilsa <hyejeekim@me.com>
2022-03-04 12:50:00 +0900

    feat: Create MoodButton

---
+++ View/MoodButton/MoodButton.swift
@@ -0,0 +1,20 @@
+//
+//  MoodButton.swift
+//  Recorday
+//
+//  Created by HyeJee Kim on 2022/03/04.
+//
+
+import UIKit
+
+class MoodButton: UIImageView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}

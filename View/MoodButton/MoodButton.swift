421053a8ade384b63218f1ac731bec021cf3d826
samsamilsa <hyejeekim@me.com>
2022-03-04 13:43:28 +0900

    feat: Make MoodButton responds to touch event

--- View/MoodButton/MoodButton.swift
+++ View/MoodButton/MoodButton.swift
@@ -8,13 +8,78 @@
 import UIKit
 
 class MoodButton: UIImageView {
-
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    
+    // MARK: - Property
+    
+    var isSelected: Bool = false {
+        didSet {
+            updateImage()
+        }
     }
-    */
+    
+    @IBInspectable var normalImage: UIImage?
+    @IBInspectable var selectedImage: UIImage?
+    
+    // MARK: - Initialize
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        print("--- \(#function) ---")
+        self.isUserInteractionEnabled = true
+    }
+    
+    // MARK: - Deinitialize
+    
+    deinit {
+        if normalImage != nil {
+            normalImage = nil
+        }
+        
+        if selectedImage != nil {
+            selectedImage = nil
+        }
+        
+        if self.image != nil {
+            self.image = nil
+        }
+        
+        print("--- \(#function) ---")
+    }
+
+    // MARK: - Responding to Touch Events
+
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        print("--- \(#function) ---")
+        
+        defer {
+            self.updateImage()
+        }
+        
+        if self.isSelected {
+            self.isSelected = false
+        }
+        else {
+            self.isSelected = true
+        }
+    }
+    
+    // MARK: -
+    
+    func updateImage() {
+        if isSelected {
+            guard let selectedImage = selectedImage else {
+                return
+            }
+
+            self.image = selectedImage
+        }
+        else {
+            guard let normalImage = normalImage else {
+                return
+            }
+            
+            self.image = normalImage
+        }
+    }
 
 }
